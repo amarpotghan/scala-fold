c4d73e0d472a39147f0aef7c16946312ab817bef
Amar <amar@capital-match.com>
2016-07-06 18:00:27 +0800

    Add one more usual apply and implement extract

--- src/main/scala/Fold.scala
+++ src/main/scala/Fold.scala
@@ -1,8 +1,10 @@
 package Foldl
 
-sealed class Foldl[B, A](val step: B => Foldl[B, A], done: Unit => A) {
+sealed class Foldl[B, A](val step: B => Foldl[B, A], val done: Unit => A) {
   def foldl(xs: List[B]): A = (xs.foldLeft(this)((f : Foldl[B, A], b) => f.step(b))).extract
-  def extract: A = ???
+  def extract: A = done(())
+
+  // def ap(other: Foldl[B, A => C]): Fold[B, C] = Foldl(ap())
 }
 
 object Foldl {
@@ -12,7 +14,9 @@ object Foldl {
   }
 
   def apply[B, A](istep: A => B => A, init: A): Foldl[B, A] = Foldl(istep, init, (identity : A => A))
-  def apply[S, B, A](istep: (S, B) => S, init: S, done: S => A): Foldl[B, A] = Foldl(istep.curried, init, done)
-  def apply[B, A](istep: (A, B) => A, init: A): Foldl[B, A] = Foldl(istep.curried, init, (identity : A => A))
+
+  def apply[S, B, A](init: S)(istep: (S, B) => S, done: S => A): Foldl[B, A] = Foldl(istep.curried, init, done)
+  def apply[B, A](init: A)(istep: (A, B) => A): Foldl[B, A] = Foldl(istep.curried, init, (identity : A => A))
+  def apply[B, A](s: B => Foldl[B, A], done: Unit => A): Foldl[B, A] = new Foldl(s, done)
 
  }
