30ba17e2795f7742c693d3c6f3d14f8ace6cf555
Amar <amar@capital-match.com>
2016-07-06 17:36:23 +0800

    Add basic structure for Fold type

--- src/main/scala/Fold.scala
+++ src/main/scala/Fold.scala
@@ -1,1 +1,18 @@
-amarp@Amars-MBP.hackerspacesg.256
+package Foldl
+
+sealed class Foldl[B, A](val step: B => Foldl[B, A], done: Unit => A) {
+  def foldl(xs: List[B]): A = (xs.foldLeft(this)((f : Foldl[B, A], b) => f.step(b))).extract
+  def extract: A = ???
+}
+
+object Foldl {
+  def apply[S, B, A](istep: S => B => S, init: S, done: S => A): Foldl[B, A] = {
+    def construct(init1: S): Foldl[B, A] = new Foldl(b => construct(istep(init1)(b)), (x => done(init1)))
+    construct(init)
+  }
+
+  def apply[B, A](istep: A => B => A, init: A): Foldl[B, A] = Foldl(istep, init, (identity : A => A))
+  def apply[S, B, A](istep: (S, B) => S, init: S, done: S => A): Foldl[B, A] = Foldl(istep.curried, init, done)
+  def apply[B, A](istep: (A, B) => A, init: A): Foldl[B, A] = Foldl(istep.curried, init, (identity : A => A))
+
+ }
