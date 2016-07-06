22f6844062ebc6d6bf5f9fc1dcc349be75128a73
Amar <amar@capital-match.com>
2016-07-06 23:16:36 +0800

    Change argument of list of one of the apply functions

--- src/main/scala/Fold.scala
+++ src/main/scala/Fold.scala
@@ -13,7 +13,7 @@ sealed class Foldl[B, A](val step: B => Foldl[B, A], val done: Unit => A) {
   def duplicate: Foldl[B, Foldl[B, A]] = this.map(Function.const(this))
 }
 
-object Foldl {
+object Foldl extends FoldlFunctions {
   def apply[S, B, A](istep: S => B => S, init: S, done: S => A): Foldl[B, A] = {
     def construct(init1: S): Foldl[B, A] = new Foldl(b => construct(istep(init1)(b)), (x => done(init1)))
     construct(init)
@@ -21,7 +21,7 @@ object Foldl {
 
   def apply[B, A](istep: A => B => A, init: A): Foldl[B, A] = Foldl(istep, init, (identity : A => A))
 
-  def apply[S, B, A](init: S)(istep: (S, B) => S, done: S => A): Foldl[B, A] = Foldl(istep.curried, init, done)
+  def apply[S, B, A](istep: (S, B) => S, init: S, done: S => A): Foldl[B, A] = Foldl(istep.curried, init, done)
   def apply[B, A](init: A)(istep: (A, B) => A): Foldl[B, A] = Foldl(istep.curried, init, (identity : A => A))
   def apply[B, A](s: B => Foldl[B, A], done: Unit => A): Foldl[B, A] = new Foldl(s, done)
 
