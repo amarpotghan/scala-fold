383094ffef6b19ba7023524358082959df48fde2
Amar <amar@capital-match.com>
2016-07-17 20:32:08 +0800

    Remove some type annotations

--- src/test/scala/FoldSpec.scala
+++ src/test/scala/FoldSpec.scala
@@ -53,19 +53,21 @@ class FoldSpecs extends Specification {
             $reverseSpec
             $dedupSpec
             $avgSpec
+
+            $syntaxSpec1
          """
 
   def lengthSpec = Foldl.length[String, Int].foldl(Seq("1", "2", "3", "4"))  must_== 4
 
   def isEmptySpec1 = Foldl.isEmpty[String].foldl(Seq[String]())  must_== true
-  def isEmptySpec2 = Foldl.isEmpty[String].foldl(Seq[String](""))  must_== false
+  def isEmptySpec2 = Foldl.isEmpty[String].foldl(Seq(""))  must_== false
 
   def sumSpec = Foldl.sum[Int].foldl(Seq(1, 1, 1)) must_== 3
   def productSpec = Foldl.product[Int].foldl(Seq(3, 3, 3)) must_== 27
 
   def allSpec1 = Foldl.all(identity: Boolean => Boolean).foldl(Seq(true, true, true)) must_== true
   def allSpec2 = Foldl.all(identity: Boolean => Boolean).foldl(Seq[Boolean]()) must_== true
-  def allSpec3 = Foldl.all(identity: Boolean => Boolean).foldl(Seq[Boolean](true, false)) must_== false
+  def allSpec3 = Foldl.all(identity: Boolean => Boolean).foldl(Seq(true, false)) must_== false
   def allSpec4 = Foldl.all((x: Int) => x % 2 == 0).foldl(Seq(2, 4, 6)) must_== true
   def allSpec5 = Foldl.all((x: Int) => x % 2 == 0).foldl(Seq(2, 3, 6)) must_== false
   def allSpec6 = Foldl.all((x: Int) => x % 2 == 0).foldl(Seq[Int]()) must_== true
@@ -73,7 +75,7 @@ class FoldSpecs extends Specification {
 
   def anySpec1 = Foldl.any(identity: Boolean => Boolean).foldl(Seq(true, true, false)) must_== true
   def anySpec2 = Foldl.any(identity: Boolean => Boolean).foldl(Seq[Boolean]()) must_== false
-  def anySpec3 = Foldl.any(identity: Boolean => Boolean).foldl(Seq[Boolean](false, false)) must_== false
+  def anySpec3 = Foldl.any(identity: Boolean => Boolean).foldl(Seq(false, false)) must_== false
   def anySpec4 = Foldl.any((x: Int) => x % 2 == 0).foldl(Seq(1, 2, 3)) must_== true
   def anySpec5 = Foldl.any((x: Int) => x % 2 == 0).foldl(Seq(1, 1, 3)) must_== false
   def anySpec6 = Foldl.any((x: Int) => x % 2 == 0).foldl(Seq[Int]()) must_== false
@@ -89,16 +91,16 @@ class FoldSpecs extends Specification {
   def andSpec2 = Foldl.and.foldl(Seq(true, true)) must_== true
   def andSpec3 = Foldl.and.foldl(Seq[Boolean]()) must_== true
 
-  def headSpec1 = Foldl.head.foldl(Seq[String]("1", "2")) must_== Some("1")
+  def headSpec1 = Foldl.head.foldl(Seq("1", "2")) must_== Some("1")
   def headSpec2 = Foldl.head.foldl(Seq[String]()) must_== None
 
   def lastSpec = Foldl.last.foldl(Seq(1, 2, 3)) must_== Some(3)
   def lastOrElseSpec = Foldl.lastOrElse(0).foldl(Seq[Int]()) must_== 0
 
-  def reverseSpec = Foldl.reverse.foldl(List[Int](1, 2, 3)) must_== List(3, 2 , 1)
+  def reverseSpec = Foldl.reverse.foldl(List(1, 2, 3)) must_== List(3, 2 , 1)
 
-  def dedupSpec = Foldl.dedup.foldl(Seq[Int](1, 2, 3, 3)) must_== List(1, 2, 3)
-  def dedupOnFoldableSpec = Foldl.dedup.foldl(List[Int](1, 2, 3, 3)) must_== List(1, 2, 3)
+  def dedupSpec = Foldl.dedup.foldl(Seq(1, 2, 3, 3)) must_== List(1, 2, 3)
+  def dedupOnFoldableSpec = Foldl.dedup.foldl(List(1, 2, 3, 3)) must_== List(1, 2, 3)
 
   def avgSpec = {
     val avgFold = Foldl.sum[Double] / Foldl.length[Double, Double]
