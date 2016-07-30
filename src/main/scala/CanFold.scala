756c1baf3522b70b54b6ba353b7938fcdb53491b
Amar <amar@capital-match.com>
2016-07-30 23:07:35 +0800

    Generalise Traversable to GenTraversable in CanFold

--- src/main/scala/CanFold.scala
+++ src/main/scala/CanFold.scala
@@ -4,6 +4,7 @@ import fold._
 import scalaz.{Foldable => SFoldable}
 import scala.collection.{ GenTraversableLike => G }
 import scala.collection._
+import fold.syntax.Syntax._
 
 trait CanFold[F[_], B] {
   def fold[A](fa: F[B])(fo: Foldl[B, A]): A
@@ -13,14 +14,9 @@ object CanFold extends CanFoldInstances
 
 trait CanFoldInstances {
 
-  // implicit def ScalazFoldable[F[_]: SFoldable, B]: CanFold[F, B] = new CanFold[F, B] {
-  //   def fold[A](xs: F[B])(fo: Foldl[B, A]): A =
-  //     implicitly[SFoldable[F]].foldLeft(xs, fo)((f: Foldl[B, A], b) => f.step(b)).extract
-  // }
-
-  implicit def StdIterable[F[B] <: Traversable[B], B]: CanFold[F, B] = new CanFold[F, B] {
+  implicit def StdIterable[F[B] <: GenTraversableLike[B, F[B]], B]: CanFold[F, B] = new CanFold[F, B] {
     def fold[A](xs: F[B])(fo: Foldl[B, A]): A =
-      xs.foldLeft(fo)(_ step _).extract
+      xs.foldWith(fo)
 
   }
 }
