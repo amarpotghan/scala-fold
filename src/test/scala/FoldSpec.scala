9d3cebf74b28b47538f9f48b5439c39f2172b209
Amar <amar@capital-match.com>
2016-08-05 22:00:30 +0800

    Refactoring in tests

--- src/test/scala/FoldSpec.scala
+++ src/test/scala/FoldSpec.scala
@@ -14,39 +14,21 @@ class FoldSpecs extends Specification {
   def is = sequential ^ s2"""Fold Specs
             $lengthSpec
 
-            $isEmptySpec1
-            $isEmptySpec2
+            $isEmptySpec
 
             $sumSpec
             $productSpec
 
-            $allSpec1
-            $allSpec2
-            $allSpec3
-            $allSpec4
-            $allSpec5
-            $allSpec6
-            $allSpec7
+            $allSpec
 
-            $anySpec1
-            $anySpec2
-            $anySpec3
-            $anySpec4
-            $anySpec5
-            $anySpec6
-            $anySpec7
+            $anySpec
 
-            $orSpec1
-            $orSpec2
-            $orSpec3
+            $orSpec
 
-            $andSpec1
-            $andSpec2
-            $andSpec3
+            $andSpec
 
 
-            $headSpec1
-            $headSpec2
+            $headSpec
 
             $lastSpec
             $lastOrElseSpec
@@ -56,20 +38,16 @@ class FoldSpecs extends Specification {
             $avgSpec
 
             $containsSpec
-            $containsSpec2
-            $containsSpec3
 
 
             $doesNotContainSpec
-            $doesNotContainSpec2
-            $doesNotContainSpec3
 
             $minimumBySpec
             $maximumBySpec
 
             $cProp
             $foldMapSpec
-            $syntaxSpec1
+            $syntaxSpec
 
             $sumScanSpec
             $takeWhileSpec
@@ -78,46 +56,56 @@ class FoldSpecs extends Specification {
 
   def lengthSpec = foldLength[String, Int].foldl(Seq("1", "2", "3", "4"))  must_== 4
 
-  def isEmptySpec1 = isEmpty[String].foldl(Seq[String]())  must_== true
-  def isEmptySpec2 = isEmpty[String].foldl(Seq(""))  must_== false
+  def isEmptySpec = {
+    isEmpty[String].foldl(Seq[String]())  must_== true
+    isEmpty[String].foldl(Seq(""))  must_== false
+  }
 
   def sumSpec = sum[Int].foldl(Seq(1, 1, 1)) must_== 3
   def productSpec = product[Int].foldl(Seq(3, 3, 3)) must_== 27
 
-  def allSpec1 = all(identity: Boolean => Boolean).foldl(Seq(true, true, true)) must_== true
-  def allSpec2 = all(identity: Boolean => Boolean).foldl(Seq[Boolean]()) must_== true
-  def allSpec3 = all(identity: Boolean => Boolean).foldl(Seq(true, false)) must_== false
-  def allSpec4 = all((x: Int) => x % 2 == 0).foldl(Seq(2, 4, 6)) must_== true
-  def allSpec5 = all((x: Int) => x % 2 == 0).foldl(Seq(2, 3, 6)) must_== false
-  def allSpec6 = all((x: Int) => x % 2 == 0).foldl(Seq[Int]()) must_== true
-  def allSpec7 = all[String](Function.const(true)).foldl(Seq[String]()) must_== true
+  def allSpec = {
+    all(identity: Boolean => Boolean).foldl(Seq(true, true, true)) must_== true
+    all(identity: Boolean => Boolean).foldl(Seq[Boolean]()) must_== true
+    all(identity: Boolean => Boolean).foldl(Seq(true, false)) must_== false
+    all((x: Int) => x % 2 == 0).foldl(Seq(2, 4, 6)) must_== true
+    all((x: Int) => x % 2 == 0).foldl(Seq(2, 3, 6)) must_== false
+    all((x: Int) => x % 2 == 0).foldl(Seq[Int]()) must_== true
+    all[String](Function.const(true)).foldl(Seq[String]()) must_== true
+  }
 
-  def anySpec1 = any(identity: Boolean => Boolean).foldl(Seq(true, true, false)) must_== true
-  def anySpec2 = any(identity: Boolean => Boolean).foldl(Seq[Boolean]()) must_== false
-  def anySpec3 = any(identity: Boolean => Boolean).foldl(Seq(false, false)) must_== false
-  def anySpec4 = any((x: Int) => x % 2 == 0).foldl(Seq(1, 2, 3)) must_== true
-  def anySpec5 = any((x: Int) => x % 2 == 0).foldl(Seq(1, 1, 3)) must_== false
-  def anySpec6 = any((x: Int) => x % 2 == 0).foldl(Seq[Int]()) must_== false
-  def anySpec7 = any[String](Function.const(true)).foldl(Seq[String]()) must_== false
+  def anySpec = {
+    any(identity: Boolean => Boolean).foldl(Seq(true, true, false)) must_== true
+    any(identity: Boolean => Boolean).foldl(Seq[Boolean]()) must_== false
+    any(identity: Boolean => Boolean).foldl(Seq(false, false)) must_== false
+    any((x: Int) => x % 2 == 0).foldl(Seq(1, 2, 3)) must_== true
+    any((x: Int) => x % 2 == 0).foldl(Seq(1, 1, 3)) must_== false
+    any((x: Int) => x % 2 == 0).foldl(Seq[Int]()) must_== false
+    any[String](Function.const(true)).foldl(Seq[String]()) must_== false
+  }
 
 
-  def orSpec1 = or.foldl(Seq(true, true, false)) must_== true
-  def orSpec2 = or.foldl(Seq(false, false)) must_== false
-  def orSpec3 = or.foldl(Seq[Boolean]()) must_== false
+  def orSpec = {
+    or.foldl(Seq(true, true, false)) must_== true
+    or.foldl(Seq(false, false)) must_== false
+    or.foldl(Seq[Boolean]()) must_== false
+  }
 
 
-  def andSpec1 = and.foldl(Seq(true, true, false)) must_== false
-  def andSpec2 = and.foldl(Seq(true, true)) must_== true
-  def andSpec3 = and.foldl(Seq[Boolean]()) must_== true
+  def andSpec = {
+    and.foldl(Seq(true, true, false)) must_== false
+    and.foldl(Seq(true, true)) must_== true
+    and.foldl(Seq[Boolean]()) must_== true
+  }
 
-  def headSpec1 = head.foldl(Seq("1", "2")) must_== Some("1")
-  def headSpec2 = head.foldl(Seq[String]()) must_== None
+  def headSpec = {
+    head.foldl(Seq("1", "2")) must_== Some("1")
+    head.foldl(Seq[String]()) must_== None
+  }
 
   def lastSpec = last.foldl(Seq(1, 2, 3)) must_== Some(3)
   def lastOrElseSpec = lastOrElse(0).foldl(Seq[Int]()) must_== 0
-
   def reverseSpec = reverse.foldl(List(1, 2, 3)) must_== List(3, 2 , 1)
-
   def dedupSpec = dedup.foldl(Seq(1, 2, 3, 3)) must_== List(1, 2, 3)
   def dedupOnFoldableSpec = dedup.foldl(List(1, 2, 3, 3)) must_== List(1, 2, 3)
 
@@ -126,7 +114,7 @@ class FoldSpecs extends Specification {
     avgFold.foldl(Seq(1.0, 2.0, 3.0, 4.0, 5.0)) must_== 3.0
   }
 
-  def syntaxSpec1 = {
+  def syntaxSpec = {
     Seq(1, 2, 3).foldWith(foldLength[Int, Int]) must_== 3
 
     def avg = sum[Double] / foldLength[Double, Double]
@@ -135,14 +123,18 @@ class FoldSpecs extends Specification {
   }
 
 
-  def containsSpec = contains(1).foldl(Seq(1, 2, 3)) must_== true
-  def containsSpec2 = contains(4).foldl(Seq(1, 2, 3)) must_== false
-  def containsSpec3 = contains(4).foldl(Seq[Int]()) must_== false
+  def containsSpec = {
+    contains(1).foldl(Seq(1, 2, 3)) must_== true
+    contains(4).foldl(Seq(1, 2, 3)) must_== false
+    contains(4).foldl(Seq[Int]()) must_== false
+  }
 
 
-  def doesNotContainSpec = doesNotContain(1).foldl(Seq(1, 2, 3)) must_== false
-  def doesNotContainSpec2 = doesNotContain(4).foldl(Seq(1, 2, 3)) must_== true
-  def doesNotContainSpec3 = doesNotContain(4).foldl(Seq[Int]()) must_== true
+  def doesNotContainSpec = {
+    doesNotContain(1).foldl(Seq(1, 2, 3)) must_== false
+    doesNotContain(4).foldl(Seq(1, 2, 3)) must_== true
+    doesNotContain(4).foldl(Seq[Int]()) must_== true
+  }
 
   def cProp = doesNotContain(-1331431553).foldl(Seq[Int]()) must_== ! (contains(-1331431553).foldl(Seq[Int]()))
 
